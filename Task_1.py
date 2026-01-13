f0b1a7e5c5e3741b3662656a5fae86c4fecc2df8
Harish Kumar Giri <harry159cool@gmail.com>
2026-01-14 01:16:40 +0530

    Add Task 1 and Task 2 solutions

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+"""Task 1: Perform Basic Mathematical Operations"""
+
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+sum1 = int(x + y)
+sub = int(x - y)
+multiply = int(x * y)
+div = x / y
+print(" ")
+print("Addition:", sum1)
+print("Subtraction:", sub)
+print("Multiplication:", multiply)
+print("Division:", div)
---
+++ Task_2.py
@@ -0,0 +1,7 @@
+"""Task 2: Create a Personalized Greeting"""
+
+fname = str(input("Enter your first name: "))
+lname = str(input("Enter your last name: "))
+name = fname + " " + lname + "!"
+print(" ")
+print("Hello,", name, "Welcome to the Python program.")
